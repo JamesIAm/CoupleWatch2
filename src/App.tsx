4f250d8cb44065a3d206e9585796e2b1ae4b51f5
James Nahajski <james.nahajski@gmail.com>
2024-09-09 19:09:24 +0100

    User adjustable search

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { generateClient } from "aws-amplify/data";
 
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import Search from "./components/Search";
 
 const client = generateClient<Schema>();
 
@@ -26,33 +27,8 @@ function App() {
 			{({ signOut }) => {
 				return (
 					<main>
-						<h1>My todos</h1>
-						<button onClick={createTodo}>+ new</button>
-						<ul>
-							{todos.map((todo) => (
-								<li key={todo.id}>{todo.content}</li>
-							))}
-						</ul>
-						<div>
-							🥳 App successfully hosted. Try creating a new todo.
-							<br />
-							<a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-								Review next step of this tutorial.
-							</a>
-						</div>
+						<Search />
 						<button onClick={signOut}>Sign out</button>
-						<button
-							onClick={async () => {
-								let output = await client.queries.searchTvShows(
-									{
-										query: "loki",
-									}
-								);
-								console.log(output);
-							}}
-						>
-							Search
-						</button>
 					</main>
 				);
 			}}
---
+++ src/components/Search.tsx
@@ -0,0 +1,31 @@
+import { generateClient } from "aws-amplify/api";
+import { FormEvent, useState } from "react";
+import { Schema } from "../../amplify/data/resource";
+
+const client = generateClient<Schema>();
+const Search = () => {
+	const [searchTerm, setSearchTerm] = useState("");
+
+	const search = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log(searchTerm);
+		let output = await client.queries.searchTvShows({
+			query: searchTerm,
+		});
+		console.log(output);
+	};
+	return (
+		<>
+			<h1>Search</h1>
+			<form onSubmit={search}>
+				<input
+					name="query"
+					onChange={(e) => setSearchTerm(e.target.value)}
+				/>
+				<button type="submit">Search</button>
+			</form>
+		</>
+	);
+};
+
+export default Search;
