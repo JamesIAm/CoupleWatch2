87ea184b3caf289766b25fed700368cddd12beb8
James Nahajski <james.nahajski@gmail.com>
2024-09-26 23:54:21 +0100

    standardise aws error logic

---
+++ src/utils/ClientUtils.ts
@@ -0,0 +1,25 @@
+type AwsResponse<Type> = {
+	data?: Type | null;
+	errors?: AwsError[];
+	extensions?: {
+		[key: string]: any;
+	};
+};
+type AwsError = {
+	message: string;
+};
+
+export const logErrorsAndReturnData = <Type>(
+	result: AwsResponse<Type>
+): Type => {
+	if (result.data) {
+		return result.data;
+	}
+	if (result.errors) {
+		result.errors.forEach((error) => {
+			console.error(error);
+		});
+		throw new Error(result.errors[0].message);
+	}
+	throw new Error("No data or errors");
+};
