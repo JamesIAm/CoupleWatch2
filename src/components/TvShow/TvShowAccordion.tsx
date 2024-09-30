45aea04ec9bc4a808946e92ef25e12ca778ac708
James Nahajski <james.nahajski+github@gmail.com>
2024-09-30 10:20:33 +0100

    make the list of currently watching shows filterable by the email of the partner

--- src/components/TvShow/TvShowAccordion.tsx
+++ src/components/TvShow/TvShowAccordion.tsx
@@ -4,6 +4,7 @@ import { TvShow } from "../Search/Search";
 import { selectCurrentlyWatching } from "../CurrentlyWatching/currentlyWatchingSlice";
 import { useAppSelector } from "../../state/hooks";
 import { Partner } from "../Partners/PartnerCard";
+import React from "react";
 
 type Props = {
 	tvShows: TvShow[];
@@ -12,44 +13,39 @@ type Props = {
 
 const TvShowAccordion = ({ tvShows, watchingWith }: Props) => {
 	const currentlyWatching = useAppSelector(selectCurrentlyWatching);
-
 	const getWatchRecords = (tvShow: TvShow) => {
 		let watchRecords = currentlyWatching.filter(
 			(show) => show.mediaId === String(tvShow.id)
 		);
-		if (watchRecords.length > 1) {
-			console.debug("More than one matching record");
-			if (watchingWith) {
-				watchRecords.filter((record) => {
-					const includesAllUsers = watchingWith.reduce(
-						(
-							watchRecordIncludesPeopleWatchingWith,
-							personWatchingWith
-						) =>
-							watchRecordIncludesPeopleWatchingWith &&
-							record.with.includes(personWatchingWith.username),
-						true
-					);
-					return (
-						includesAllUsers &&
-						record.with.length === watchingWith.length
-					);
-				});
-			}
+		if (!watchingWith) {
+			return watchRecords;
 		}
-		return watchRecords;
+		return watchRecords.filter((record) => {
+			const includesAllUsers = watchingWith.reduce(
+				(watchRecordIncludesPeopleWatchingWith, personWatchingWith) =>
+					watchRecordIncludesPeopleWatchingWith &&
+					record.with.includes(personWatchingWith.username),
+				true
+			);
+			return includesAllUsers;
+		});
 	};
 
 	const getAllListingsForATvShow = (tvShow: TvShow, index: number) => {
 		let watchRecords = getWatchRecords(tvShow);
+
 		if (watchRecords.length === 0) {
-			return (
-				<TvShowAccordionItem
-					data={tvShow}
-					watchRecord={null}
-					key={tvShow.id}
-				/>
-			);
+			if (watchingWith) {
+				return <React.Fragment key={index}></React.Fragment>;
+			} else {
+				return (
+					<TvShowAccordionItem
+						data={tvShow}
+						watchRecord={null}
+						key={tvShow.id}
+					/>
+				);
+			}
 		}
 		return (
 			<div key={index}>
