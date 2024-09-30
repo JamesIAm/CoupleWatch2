90114202e6f6be1ca411a93ab705cc82e3287108
James Nahajski <james.nahajski+github@gmail.com>
2024-09-30 14:33:41 +0100

    setting a custom auth domain
    This has been changed manually in Cognito, and registered there

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import outputs from "../amplify_outputs.json";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { Provider } from "react-redux";
 import { store } from "./state/store.ts";
-
+outputs.auth.oauth.domain = import.meta.env.VITE_OAUTH_DOMAIN;
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
