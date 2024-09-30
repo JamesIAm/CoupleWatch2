d8b6d2c47e112f88fe68882f23b8e13ca4e7aa46
James Nahajski <james.nahajski+github@gmail.com>
2024-09-30 23:37:53 +0100

    Showing a failure message when a user search fails

--- src/components/Partners/PartnerSearch.tsx
+++ src/components/Partners/PartnerSearch.tsx
@@ -1,4 +1,9 @@
-import { SearchField, useAuthenticator } from "@aws-amplify/ui-react";
+import {
+	Card,
+	Placeholder,
+	SearchField,
+	useAuthenticator,
+} from "@aws-amplify/ui-react";
 import { generateClient } from "aws-amplify/api";
 import { Schema } from "../../../amplify/data/resource";
 import { useState } from "react";
@@ -9,21 +14,48 @@ type Props = {};
 const client = generateClient<Schema>();
 
 const PartnerSearch = ({}: Props) => {
-	const [searchedPartner, setSearchedPartner] = useState<Partner | undefined>(
-		undefined
-	);
+	const [searchedPartner, setSearchedPartner] = useState<
+		Partner | null | undefined
+	>(undefined);
+	const [searching, setSearching] = useState<boolean>(false);
 
 	const { user } = useAuthenticator((context) => [context.user]);
 	console.log(searchedPartner);
 	const searchForUser = (email: string) => {
+		setSearching(true);
 		client.queries.searchUser({ email: email }).then((res) => {
-			console.log(res);
+			setSearching(false);
+			if (res.errors) {
+				throw new Error(res.errors[0].message);
+			}
 			if (res.data) {
 				setSearchedPartner({ email: email, username: res.data });
 			} else {
-				setSearchedPartner(undefined);
+				setSearchedPartner(null);
 			}
 		});
+	};
+
+	const renderSearchedPartner = () => {
+		{
+			if (searching) {
+				return <Placeholder size="large" />;
+			}
+			if (searchedPartner === undefined) {
+				return <></>;
+			}
+			if (
+				searchedPartner === null ||
+				(searchedPartner && searchedPartner.username === user.username)
+			) {
+				return (
+					<Card>
+						Not found, ensure you search by their full email address
+					</Card>
+				);
+			}
+			return <PartnerCard partner={searchedPartner} />;
+		}
 	};
 
 	return (
@@ -31,16 +63,13 @@ const PartnerSearch = ({}: Props) => {
 			<h2>Find a partner</h2>
 			<SearchField
 				label="Find a partner"
+				placeholder="example@gmail.com"
 				hasSearchIcon={true}
 				onSubmit={(searchTerm) => searchForUser(searchTerm)}
 				onClear={() => setSearchedPartner(undefined)}
 				onChange={() => setSearchedPartner(undefined)}
 			/>
-			{searchedPartner && searchedPartner.username !== user.username ? (
-				<PartnerCard partner={searchedPartner} />
-			) : (
-				<></>
-			)}
+			{renderSearchedPartner()}
 		</div>
 	);
 };
