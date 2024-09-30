f4759b89abce8af03accef836b35e160a1c33b85
James Nahajski <james.nahajski+github@gmail.com>
2024-09-30 10:20:33 +0100

    make the list of currently watching shows filterable by the email of the partner

--- src/components/CurrentlyWatching/CurrentlyWatchingList.tsx
+++ src/components/CurrentlyWatching/CurrentlyWatchingList.tsx
@@ -1,10 +1,16 @@
+import { Tabs } from "@aws-amplify/ui-react";
 import { useAppSelector } from "../../state/hooks";
+import { selectPairings } from "../Partners/pairingsSlice";
 import TvShowAccordion from "../TvShow/TvShowAccordion";
 import { selectCurrentlyWatching } from "./currentlyWatchingSlice";
+import { useState } from "react";
+import { Partner } from "../Partners/PartnerCard";
 
 type Props = {};
 const CurrentlyWatchingList = ({}: Props) => {
 	const currentlyWatching = useAppSelector(selectCurrentlyWatching);
+	const pairings = useAppSelector(selectPairings);
+	const [tab, setTab] = useState<Partner | undefined>(undefined);
 	const currentlyWatchingShows = currentlyWatching.map(
 		(watching) => watching.show
 	);
@@ -14,13 +20,33 @@ const CurrentlyWatchingList = ({}: Props) => {
 				(otherShow) => show.id === otherShow.id
 			) === index
 	);
-	console.log(uniqueCurrentlyWatchingShows);
 	return (
 		<div>
 			<h1>CurrentlyWatchingList</h1>
+			<Tabs
+				onValueChange={(tab) => {
+					if (tab === "All") setTab(undefined);
+					setTab(pairings.find((pairing) => pairing.email === tab));
+				}}
+				value={tab ? tab.email : "All"}
+				items={[
+					{
+						label: "All",
+						value: "All",
+						content: <></>,
+					},
+					...pairings.map((partner) => {
+						return {
+							label: partner.email,
+							value: partner.email,
+							content: <></>,
+						};
+					}),
+				]}
+			/>
 			<TvShowAccordion
 				tvShows={[...uniqueCurrentlyWatchingShows]}
-				watchingWith={undefined}
+				watchingWith={tab ? [tab] : tab}
 			/>
 		</div>
 	);
